34a6a1fe92a5f863712cb2a65644c346ef995a51
Eduar Romero <eg.romeror1@uniandes.edu.co>
2023-11-11 06:58:50 -0500

    refactorización, usuario y contraseña cualquier, no exponer

--- cypress/e2e/signin/signin.cy.js
+++ cypress/e2e/signin/signin.cy.js
@@ -1,7 +1,7 @@
 import { registerCommands } from '../../../pruebas-cypress/cypress/support/commands'
 registerCommands()
-let user = 'eg.romeror1@uniandes.edu.co'
-let passw = 'GioR2023@@'
+let user = 'xxxxxxxx'
+let passw = 'xxxxxxxxxx'
 describe ('Get in Post', function(){
     it('Ingreso a la opción de post', function(){
         cy.login(user,passw)
