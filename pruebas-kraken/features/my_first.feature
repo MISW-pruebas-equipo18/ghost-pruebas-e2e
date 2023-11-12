15cb9b266ae35ca782a1002b65467f4af8d9d972
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2023-11-12 10:21:13 -0500

    Add first scenario
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

--- pruebas-kraken/features/my_first.feature
+++ pruebas-kraken/features/my_first.feature
@@ -4,12 +4,27 @@ Feature: Kraken Scenarios
 
 @user1 @web
 Scenario: Pruebas exploratorias
+
+  # Escenario 1: Publicar y editar un Post
   Given I navigate to page "http://localhost:2368/ghost/#/signin"
-  And I wait for 5 seconds
+  And I wait for 2 seconds
   And I login with "<USERNAME>" user and "<PASSWORD>" password
-
-  # Scenario 1
-
-  # Scenario 2
-
-
+  And I wait for 2 seconds
+  And Title is "Dashboard"
+  And I wait for 2 seconds
+  When I go to posts view
+  And I wait for 2 seconds
+  And I go to new post view
+  And I wait for 2 seconds
+  And I write a new post with title "Test Post scenario 1" and body "Test Body scenario 1"
+  And I wait for 2 seconds
+  And I publish the post
+  And I wait for 2 seconds
+  And I go back to posts view
+  Then I should see a post with title "Test Post scenario 1"
+  And I wait for 5 seconds
+  # Tear down
+  And I delete the post with title "Test Post scenario 1" 
+  And I wait for 2 seconds
+  And I logout
+  And I wait for 2 seconds
--- pruebas-kraken/features/web/step_definitions/step.js
+++ pruebas-kraken/features/web/step_definitions/step.js
@@ -11,3 +11,101 @@ When('I login with {kraken-string} user and {kraken-string} password', async fun
     let loginButton = await this.driver.$('button.login');
     await loginButton.click();
 });
+
+Then('I logout', async function () {
+    let avatarButton = await this.driver.$('div[class="pe-all"] > div:first-child');
+    await avatarButton.click();
+
+    let signoutButton = await this.driver.$('a[href="#/signout/"]');
+    await signoutButton.click();
+});
+
+When('Title is {string}', async function (pageTitle) {
+   let currentTitle = await this.driver.$('h2.gh-canvas-title'); 
+   expect(await currentTitle.getText()).to.equal(pageTitle);
+});
+
+When('I go to posts view', async function () {
+    let postsLink = await this.driver.$('a[href="#/posts/"]'); // id: ember9
+    await postsLink.click();
+});
+
+When('I go to new post view', async function () {
+    let newPostButton = await this.driver.$('a[href="#/editor/post/"]');
+    await newPostButton.click();
+});
+
+When('I write a new post with title {string} and body {string}', async function (title, body) {
+    let titleInput = await this.driver.$('textarea.gh-editor-title');
+    await titleInput.setValue(title);
+
+    // Wait 1 second
+    
+    let bodyInput = await this.driver.$('div[class="kg-prose"] > p');
+    bodyInput.click();
+
+    await this.driver.pause(1000);
+    await bodyInput.setValue(body);
+});
+
+When('I publish the post', async function () {
+    let publishButton = await this.driver.$('button[class="gh-btn gh-btn-editor darkgrey gh-publish-trigger"]')
+    await publishButton.click();
+
+    let publishButton2 = await this.driver.$('button[data-test-button="continue"]');
+    await publishButton2.click();
+
+    let publishButton3 = await this.driver.$('button[data-test-button="confirm-publish"]');
+    await publishButton3.click();
+});
+
+When('I go back to posts view', async function () {
+    let back1 = await this.driver.$('button.gh-publish-back-button')
+    await back1.click();
+
+    let back2 = await this.driver.$('a[href="#/posts/"]')
+    await this.driver.pause(1000);
+    await back2.click();
+});
+
+Then('I should see a post with title {string}', async function (title) {
+    // let titleElement = await this.driver.$('h3.gh-content-entry-title');
+    // expect(await titleElement.getText()).to.equal(title);
+
+    let titleElements = await this.driver.$$('h3.gh-content-entry-title');
+
+    // console.log('titleElements', titleElements);
+
+    let found = false;
+    for (let titleElement of titleElements) {
+        if (await titleElement.getText() == title) {
+            found = true;
+            break;
+        }
+    }
+
+    expect(found).to.be.true;
+});
+
+When('I delete the post with title {string}', async function (title) {
+    let titleElements = await this.driver.$$('h3.gh-content-entry-title');
+
+    let found = false;
+    for (let titleElement of titleElements) {
+        if (await titleElement.getText() == title) {
+            found = true;
+            let parent = await titleElement.$('../..');
+            // Right click parent
+            await parent.click({ button: 'right' });
+
+            let deleteButton = await this.driver.$('span[class="red"]');
+            await deleteButton.click();
+
+            let confirmButton = await this.driver.$('button[class="gh-btn gh-btn-red gh-btn-icon ember-view"]');
+            await confirmButton.click();
+            break;
+        }
+    }
+
+    expect(found).to.be.true;
+});
